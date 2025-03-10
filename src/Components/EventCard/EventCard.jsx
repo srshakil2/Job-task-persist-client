d54a0876e5e729805dcd37fb26fd036384adac72
srshakil2 <rh3642921@gmail.com>
2025-03-11 05:50:59 +0600

    post oparation event data success

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,4 +1,4 @@
-const EventCard = ({ item, refetch }) => {
+const EventCard = ({ item, addEvent }) => {
   //   console.log(item);
   return (
     <div className="relative max-w-sm p-5 bg-white shadow-xl rounded-2xl border border-gray-200 transition-transform transform hover:scale-105 duration-300">
@@ -7,10 +7,15 @@ const EventCard = ({ item, refetch }) => {
       <p className="text-sm text-gray-500">
         {item.Date} • {item.Location}
       </p>
-      <p className="mt-2 text-gray-700">{item.Description}</p>
+      <p className="mt-2 text-gray-700">
+        {item.Description.split(" ").slice(0, 25).join(" ")}
+      </p>
       {/* Add Add New Events btn */}
       <div className="inline-block mt-10">
-        <button className="btn fixed right-4 bottom-3  bg-indigo-800 text-white hover:bg-indigo-900 border-none  shadow-none ">
+        <button
+          onClick={addEvent}
+          className="btn fixed right-4 bottom-3  bg-indigo-800 text-white hover:bg-indigo-900 border-none  shadow-none "
+        >
           Add New Events
         </button>
       </div>
--- src/Components/EventList/EventList.jsx
+++ src/Components/EventList/EventList.jsx
@@ -1,14 +1,148 @@
 import useAllData from "../../Hooks/useAllData";
 import EventCard from "../EventCard/EventCard";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const EventList = () => {
   const [data, refetch] = useAllData("/allData");
   // console.log(data);
+
+  const handelModalData = (e) => {
+    const data = e.target;
+    const title = data.title.value;
+    const date = data.date.value;
+    const description = data.description.value;
+    const location = data.location.value;
+    const category = data.category.value;
+    if (category == "") return;
+    const inputData = {
+      Title: title,
+      Date: date,
+      Location: location,
+      Description: description,
+      Category: category,
+    };
+    // console.log(inputData);
+    axios
+      .post("http://localhost:5000/eventData", inputData)
+      .then((res) => {
+        if (res.data?.insertedId) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Event data save success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+          data.reset();
+        }
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
+  };
+  const addEvent = () => {
+    document.getElementById("my_modal_5").showModal();
+  };
+
+  // location
+
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-5 px-5 mb-10  justify-center ">
-      {data.map((item) => (
-        <EventCard key={item._id} item={item} refetch={refetch}></EventCard>
-      ))}
+    <div>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-5 px-5 mb-10  justify-center ">
+        {data.map((item) => (
+          <EventCard key={item._id} item={item} addEvent={addEvent}></EventCard>
+        ))}
+      </div>
+      {/* modal */}
+      <div className="relative ">
+        <dialog
+          id="my_modal_5"
+          className="modal modal-bottom sm:modal-middle px-5 md:px-0"
+        >
+          <div className="modal-box overflow-hidden">
+            <button
+              onClick={() => document.getElementById("my_modal_5").close()}
+              className="btn fixed -top-1 -right-1 rounded-full bg-red-400"
+            >
+              X
+            </button>
+
+            {/* modal-action */}
+            <div className="">
+              <form method="dialog" onSubmit={(e) => handelModalData(e)}>
+                {/* if there is a button in form, it will close the modal */}
+                <h2 className="text-xl font-bold mb-4">
+                  Enter Your Event Details
+                </h2>
+                {/* title */}
+                <div className="mb-4">
+                  <label className="block text-sm font-medium">Title:</label>
+                  <input
+                    type="text"
+                    name="title"
+                    placeholder="Enter Title"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* date */}
+                <div className="mb-4">
+                  <label className="block text-sm font-medium">Date:</label>
+                  <input
+                    type="date"
+                    name="date"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* location */}
+                <div className="mb-4">
+                  <label className="block text-sm font-medium">Location:</label>
+                  <input
+                    type="text"
+                    name="location"
+                    placeholder="Enter Location"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* description */}
+                <div className="mb-4">
+                  <label className="block text-sm font-medium">
+                    Description:
+                  </label>
+                  <textarea
+                    name="description"
+                    placeholder="Enter Description"
+                    className="textarea textarea-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* select option */}
+                <div className="mb-4">
+                  <label className="block text-sm font-medium mb-2">
+                    Category:
+                  </label>
+                  <select
+                    name="category"
+                    className="select select-bordered w-full"
+                  >
+                    <option value="">Select a category</option>
+                    <option value="Religious">Religious</option>
+                    <option value="Social">Social</option>
+                    <option value="Charity">Charity</option>
+                  </select>
+                </div>
+                <button className="btn w-full bg-indigo-800 hover:bg-indigo-700 text-white">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </div>
   );
 };
